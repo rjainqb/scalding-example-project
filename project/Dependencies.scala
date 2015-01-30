d98379d8dc7378a4557278b97164364e81d27f57
Rajat Jain <rjain@qubole.com>
2015-01-30 15:36:06 -0800

    Make the scalding project work with qubole's hadoop jars

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,19 +15,21 @@ import sbt._
 object Dependencies {
   val resolutionRepos = Seq(
     "ScalaTools snapshots at Sonatype" at "https://oss.sonatype.org/content/repositories/snapshots/",
-    "Concurrent Maven Repo" at "http://conjars.org/repo" // For Scalding, Cascading etc
+    "Concurrent Maven Repo" at "http://conjars.org/repo",
+    "Qubole Maven Snapshot Repo" at "http://maven-qubole.s3-website-us-east-1.amazonaws.com/maven/snapshot",
+    "Qubole Maven Release Repo" at "http://maven-qubole.s3-website-us-east-1.amazonaws.com/maven/release"
   )
 
   object V {
     val scalding  = "0.9.1"
-    val hadoop    = "1.1.2"
+    val hadoop    = "0.20.1-dev"
     val specs2    = "2.3.11"
     // Add versions for your additional libraries here...
   }
 
   object Libraries {
     val scaldingCore = "com.twitter"                %%  "scalding-core"       % V.scalding
-    val hadoopCore   = "org.apache.hadoop"          %   "hadoop-core"         % V.hadoop       % "provided"
+    val hadoopCore   = "com.qubole"                 %   "hadoop"              % V.hadoop       % "provided"
     // Add additional libraries from mvnrepository.com (SBT syntax) here...
 
     // Scala (test only)
